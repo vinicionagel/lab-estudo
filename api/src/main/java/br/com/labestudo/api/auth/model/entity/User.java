5374c0e7b53ea3fb152e7c3b706212e04f690817
Brian Bienemann <brian@agilliza.com>
2021-05-01 12:05:03 -0300

    add oauth password flow

---
+++ api/src/main/java/br/com/labestudo/api/auth/model/entity/User.java
@@ -0,0 +1,39 @@
+package br.com.labestudo.api.auth.model.entity;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String pass;
+	
+	private Boolean active;
+	
+	@CreationTimestamp
+	private OffsetDateTime created;
+	
+	@UpdateTimestamp
+	private OffsetDateTime updated; 
+
+}
