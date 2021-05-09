658ef1d5d096fa370865d9bbb4f4b05866d726cc
Brian Bienemann <brian@agilliza.com>
2021-05-09 19:09:05 -0300

    add authorization server

--- api/src/main/java/br/com/labestudo/api/auth/config/AuthorizationServerConfig.java
+++ api/src/main/java/br/com/labestudo/api/auth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package br.com.labestudo.api.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,7 +10,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
+import br.com.labestudo.api.auth.properties.JwtKeyStoreProperties;
 import br.com.labestudo.api.auth.service.JpaUserDetailsService;
 
 @Configuration
@@ -24,6 +32,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private JpaUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtKeyStoreProperties jwtKeyStoreProperties;
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -48,9 +59,32 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 			.authenticationManager(authenticationManager)
-			.userDetailsService(userDetailsService);
+			.userDetailsService(userDetailsService)
+			.reuseRefreshTokens(false)
+			.accessTokenConverter(jwtAccessTokenConverter())
+			.approvalStore(approvalStore(endpoints.getTokenStore()));
 	}
 
-	
+	private ApprovalStore approvalStore(TokenStore tokenStore) {
+		var tokenApprovalStore = new TokenApprovalStore();
+		tokenApprovalStore.setTokenStore(tokenStore);
+		
+		return tokenApprovalStore;
+	}
+
+	@Bean
+	public AccessTokenConverter jwtAccessTokenConverter() {
+		var jksResource = jwtKeyStoreProperties.getPath();
+		var keyPairAlias = jwtKeyStoreProperties.getKeyPairAlias();
+		var keyStorePass = jwtKeyStoreProperties.getKeyStorePass().toCharArray();
+		
+		var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass);
+		var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+		
+		var jwtAccessTokenConverter= new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setKeyPair(keyPair);
+		
+		return jwtAccessTokenConverter;
+	}
 	
 }
