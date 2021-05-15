a2d686b580bc6dedb8a1e07af6492050e2e67e95
Emerson Torres <emerson.torres.tec@gmail.com>
2021-05-15 11:38:44 -0300

    ajuste imports

--- api/src/main/java/br/com/labestudo/api/auth/config/CorsConfig.java
+++ api/src/main/java/br/com/labestudo/api/auth/config/CorsConfig.java
@@ -10,9 +10,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Configuration
