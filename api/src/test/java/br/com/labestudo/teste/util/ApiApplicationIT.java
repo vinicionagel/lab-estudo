5b60bfc6a87d3d290cda3b127e46a7e14856b7a1
vinicionagel <vinicionagel@gmail.com>
2021-07-08 22:21:29 -0300

    conteiner por dentro da aplicação

--- api/src/test/java/br/com/labestudo/teste/util/ApiApplicationIT.java
+++ api/src/test/java/br/com/labestudo/teste/util/ApiApplicationIT.java
@@ -3,9 +3,31 @@ package br.com.labestudo.teste.util;
 import br.com.labestudo.api.ApiApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest(classes = ApiApplication.class)
 @ActiveProfiles("test")
-public class ApiApplicationIT {
+public abstract class ApiApplicationIT {
+
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:13.1")
+            .withUsername("test")
+            .withPassword("test")
+            .withDatabaseName("test");
+
+    static {
+        postgreSQLContainer.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            postgreSQLContainer.stop();
+        }));
+    }
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
 }
